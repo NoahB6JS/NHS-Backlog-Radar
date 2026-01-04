7ca4bc68b1808a2862f4dd811d0116a044910e0f
NoahB6JS <noah.js.b6@gmail.com>
2026-01-04 19:44:40 +0000

    main.py update

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ import pandas as pd
 
 file_path = "Data_sets/workforce_database.xlsx"
 xls = pd.ExcelFile(file_path)
-
 df = pd.read_excel(file_path, sheet_name="1. England", skiprows=9)
-
 print(df.head(20))
 
 #Store data
 
-df.to_csv("nhs_staffing_raw.csv", index=True)
+df.fillna(0, inplace=True) 
+df.to_csv("nhs_staffing.csv", index=True)
 
 #Clean data
 
-
+ # replace empty cells with 0
 
 #Organise data and transform it 
 
